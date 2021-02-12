ba305051ce6e3182173b128942f83d8ff0d2fd31
Naoki Yahata <funkinaoki@gmail.com>
2021-02-12 22:34:48 +0900

    Initial Commit

---
+++ Task3typesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task3typesApp
+//
+//  Created by 八幡尚希 on 2021/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
